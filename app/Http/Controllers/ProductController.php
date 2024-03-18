05cdd13c44820e0c8e06a931e4bc4fb7418cb3f4
GeorgeC137 <omollogeorge17@gmail.com>
2024-03-18 12:50:43 +0300

    implementing sorting of products on website

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,43 +4,64 @@ namespace App\Http\Controllers;
 
 use App\Models\Category;
 use App\Models\Product;
+use Illuminate\Contracts\Database\Eloquent\Builder;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
     public function index()
     {
-        $products = Product::query()
-            ->where('published', '=', 1)
-            ->orderBy('created_at', 'desc')
-            ->paginate(5);
+        $query = Product::query();
 
-        return view('product.index', [
-            'products' => $products
-        ]);
+        return $this->renderProducts($query);
     }
 
     public function byCategory(Category $category)
     {
         $categories = Category::getAllChildrenByParent($category);
 
-        $products = Product::query()
+        $query = Product::query()
             ->select('products.*')
             ->join('product_categories AS pc', 'pc.product_id', '=', 'products.id')
-            ->where('published', '=', 1)
-            ->whereIn('pc.category_id', array_map(fn($c) => $c->id, $categories))
-            ->orderBy('created_at', 'desc')
-            ->paginate(5);
+            ->whereIn('pc.category_id', array_map(fn ($c) => $c->id, $categories));
 
-        return view('product.index', [
-            'products' => $products
-        ]);
+        return $this->renderProducts($query);
     }
 
     public function show(Product $product)
     {
         return view('product.show', [
             'product' => $product
+        ]);
+    }
+
+    private function renderProducts(Builder $query)
+    {
+        $search = request()->get('search');
+        $sort = request()->get('sort', '-updated_at');
+
+        if ($sort) {
+            $sortDirection = 'asc';
+
+            if ($sort[0] === '-') {
+                $sortDirection = 'desc';
+            }
+
+            $sortField = preg_replace('/^-?/', '', $sort);
+
+            $query->orderBy($sortField, $sortDirection);
+        }
+
+        $products = $query
+            ->where('published', '=', 1)
+            ->where(function ($query) use ($search) {
+                $query->where('products.title', 'like', "%$search%")
+                    ->orwhere('products.description', 'like', "%$search%");
+            })
+            ->paginate(5);
+
+        return view('product.index', [
+            'products' => $products
         ]);
     }
 }
--- resources/views/product/index.blade.php
+++ resources/views/product/index.blade.php
@@ -1,15 +1,45 @@
 <?php
-    $categoryList = App\Models\Category::getActiveAsTree()
+$categoryList = App\Models\Category::getActiveAsTree();
 ?>
 
 <x-app-layout>
     <x-category-list :category-list="$categoryList" class="-ml-5 -mt-5 px-4 -mr-5" />
+
+    <div class="p-3 pb-0 flex items-center gap-2" x-data="{
+        selectedSort: '{{ request()->get('sort', '-updated_at') }}',
+        searchKeyword: '{{ request()->get('search') }}',
+        updateUrl() {
+            const params = new URLSearchParams(window.location.search)
+            params.set('sort', this.selectedSort)
+            if (this.searchKeyword) {
+                params.set('search', this.searchKeyword)
+            } else {
+                params.delete('search')
+            }
+            window.location.href = window.location.origin + window.location.pathname + '?' + params.toString();
+        }
+    }">
+        <form action="" method="GET" class="flex-1" @submit.prevent="updateUrl">
+            <x-text-input type="text" name="search" x-model="searchKeyword"
+                placeholder="Search products..." />
+        </form>
+        <x-text-input type="select" @change="updateUrl" x-model="selectedSort" name="sort"
+            class="w-full focus:border-purple-600 focus:ring-purple-600 border-gray-300 rounded">
+            <option value="price">Price (ASC)</option>
+            <option value="-price">Price (DESC)</option>
+            <option value="title">Title (ASC)</option>
+            <option value="-title">Title (DESC)</option>
+            <option value="-updated_at">Last Modified at the top</option>
+            <option value="updated_at">Last Modified at the bottom</option>
+        </x-text-input>
+    </div>
+
     @if ($products->count() === 0)
         <div class="text-center text-gray-600 py-16 text-xl">
             There are no products published
         </div>
     @else
-        <div class="grid gap-8 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 p-5">
+        <div class="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 p-3">
             @foreach ($products as $product)
                 <!-- Product Item -->
                 <div x-data="productItem({{ json_encode([
