8f1b82b721e038c1f6d95fb29b9c2ff7694db3f6
GeorgeC137 <omollogeorge17@gmail.com>
2024-05-03 14:50:29 +0300

    solving online image error

--- config/filesystems.php
+++ config/filesystems.php
@@ -33,7 +33,6 @@ return [
         'local' => [
             'driver' => 'local',
             'root' => storage_path('app'),
-            'visibility' => 'public',
             'throw' => false,
         ],
 
