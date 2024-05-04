0930eb0134d62e40496a84a13bb31e87708ad7d7
GeorgeC137 <omollogeorge17@gmail.com>
2024-05-04 18:30:25 +0300

    online images

--- config/filesystems.php
+++ config/filesystems.php
@@ -71,7 +71,7 @@ return [
     */
 
     'links' => [
-        base_path('public_html/storage') => storage_path('app/public'),
+        public_path('storage') => storage_path('app/public'),
     ],
 
 ];
